961a940aee489c925f2cb6cafa17cfc1d14bb800
alekwq1 <malyshkogrodno@gmail.com>
2025-06-10 14:35:02 +0200

    Twoja wiadomość opisująca zmiany

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   assetsInclude: ["**/*.gltf"],
   plugins: [react()],
-  server: {
-    headers: {
-      "Content-Security-Policy": "script-src 'self' 'unsafe-eval';",
-    },
-  },
 });
