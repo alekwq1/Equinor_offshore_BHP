85a4db1c064a2a7b1eda0e40aaadb4a2cde0aaeb
alekwq1 <malyshkogrodno@gmail.com>
2025-06-10 14:50:06 +0200

    Fix: ensure _headers exists and is copied

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
+import { viteStaticCopy } from "vite-plugin-static-copy";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
   assetsInclude: ["**/*.gltf"],
-  plugins: [react()],
-  publicDir: resolve(__dirname, "public"), // zapewnia skopiowanie do dist/
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: resolve(__dirname, "public/_headers"),
+          dest: ".", // skopiuje do dist/_headers
+        },
+      ],
+    }),
+  ],
+  publicDir: resolve(__dirname, "public"),
 });
